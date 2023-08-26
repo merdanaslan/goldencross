00e2c62515db52ed97695ec9a5c7f07e57797f5e
Merdan Aslan <m.aslan@campus.tu-berlin.de>
2023-08-26 13:15:36 +0200

    Update

---
+++ README.md
@@ -0,0 +1,41 @@
+# Goldencross-buyhold-SPY
+
+#### Note
+
+* used argparse (Command-line parsing library) to load the strategies more easily
+
+
+#### Results goldencross
+
+```
+Buy 99 shares of SPY at 95.110001
+Sell 99 shares of SPY at 110.029999
+Buy 92 shares of SPY at 117.279999
+Sell 92 shares of SPY at 125.690002
+Buy 89 shares of SPY at 130.410004
+Sell 89 shares of SPY at 148.130005
+Buy 148 shares of SPY at 89.349998
+Sell 148 shares of SPY at 102.870003
+Buy 127 shares of SPY at 118.349998
+Sell 127 shares of SPY at 118.120003
+Buy 114 shares of SPY at 131.320007
+Sell 114 shares of SPY at 199.279999
+Buy 108 shares of SPY at 204.860001
+Sell 108 shares of SPY at 192.110001
+Buy 102 shares of SPY at 208.610001
+Sell 102 shares of SPY at 263.570007
+Buy 92 shares of SPY at 285.829987
+```
+![Alt text](goldencross_spy.png)
+
+
+
+
+
+#### Results buyhold
+
+```
+10000.0
+ROI:        131.97%
+```
+![Alt text](buyhold_spy.png)
---
+++ run_strategy.py
@@ -0,0 +1,38 @@
+# import os, sys, argparse
+import argparse
+import sys
+import pandas as pd
+import backtrader as bt
+from backtrader import Cerebro
+from strategies.goldencross import GoldenCross
+from strategies.buyhold import BuyHold
+
+cerebro = bt.Cerebro()
+
+prices = pd.read_csv('data/spy_2000-2020.csv', index_col='Date', parse_dates=True)
+
+# initialize the Cerebro engine
+cerebro = Cerebro()
+cerebro.broker.setcash(10000)
+
+# add OHLC data feed
+feed = bt.feeds.PandasData(dataname=prices)
+cerebro.adddata(feed)
+
+strategies = {
+    "golden_cross": GoldenCross,
+    "buy_hold": BuyHold
+}
+
+# parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("strategy", help="Which strategy to run", type=str)
+args = parser.parse_args()
+
+if args.strategy not in strategies:
+    print("Invalid strategy, must select one of {}".format(strategies.keys()))
+    sys.exit()
+
+cerebro.addstrategy(strategy=strategies[args.strategy])
+cerebro.run()
+cerebro.plot()
